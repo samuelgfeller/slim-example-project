a2b64640ddf2a1c84d5ebc3741b4a7e0c45b1e42
Samuel Gfeller <samuelgfeller@bluewin.ch>
2019-12-29 18:31:50 +0100

    reconfigured jwtAuth middleware (added logger, ignore path, http dev paths, error return)

--- app/middleware.php
+++ app/middleware.php
@@ -2,19 +2,39 @@
 
 use App\Application\Middleware\JsonBodyParserMiddleware;
 use App\Application\Middleware\SessionMiddleware;
+use Psr\Log\LoggerInterface;
 use Slim\App;
+
 return function (App $app) {
 
-    $settings = $app->getContainer()->get('settings');
+    $container = $app->getContainer();
+
+    $settings = $container->get('settings');
+    $logger = $container->get(LoggerInterface::class);
+
 
     $app->add(SessionMiddleware::class);
     $app->add(JsonBodyParserMiddleware::class);
 
     $app->add(new \Tuupola\Middleware\JwtAuthentication([
-        "path" => "/api", /* or ["/api", "/admin"] */
+//        'path' => '/api', /* or ["/api", "/admin"] */
+        'ignore' => ['/frontend', '/login'],
         //"attribute" => "decoded_token_data",
-        "secret" => "test",//$settings['settings']['jwt']['secret'],
-        "algorithm" => ["HS256"],
+        'secret' => 'test',//$settings['settings']['jwt']['secret'],
+        'algorithm' => ['HS256'],
+        'logger' => $logger,
+
+        // HTTPS not mandatory for local development
+        'relaxed' => ['localhost', 'dev.slim_api_skeleton'],
+        "error" => function ($response, $arguments) {
+            $data["status"] = "error";
+            $data["message"] = $arguments["message"];
+            return $response
+                ->withHeader("Content-Type", "application/json")
+                ->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+        }
+
+
         /*"error" => function ($response, $arguments) {
             $data["status"] = "error";
             $data["message"] = $arguments["message"];
