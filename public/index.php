e17e5868079ea8f97bcfbdd332750af7cc3efc56
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-01-14 09:46:28 +0100

    Display error details again

--- public/index.php
+++ public/index.php
@@ -3,7 +3,6 @@
 ini_set('xdebug.var_display_max_depth', '10');
 ini_set('xdebug.var_display_max_children', '256');
 ini_set('xdebug.var_display_max_data', '1024');
-ini_set('display_errors', 'Off');
 
 use App\Application\Error\HtmlErrorRenderer;
 use Slim\Factory\AppFactory;
@@ -104,8 +103,8 @@ $routesFrontend($app);
  */
 // todo add custom error_log http://www.slimframework.com/docs/v4/middleware/error-handling.html
 
-$errorMiddleware = $app->addErrorMiddleware(false, true, true);
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
 $errorHandler = $errorMiddleware->getDefaultErrorHandler();
-$errorHandler->registerErrorRenderer('text/html', HtmlErrorRenderer::class);
+//$errorHandler->registerErrorRenderer('text/html', HtmlErrorRenderer::class);
 
 $app->run();
