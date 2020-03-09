c1f3e260494bd1630a2b536d9e1620c8080faad4
Samuel Gfeller <sgfeller@wmc.ch>
2020-03-09 16:12:52 +0100

    Removed domain exception and added new exception path with ValidationException.php

---
+++ src/Exception/ValidationException.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Exception;
+
+use App\Domain\Validation\ValidationResult;
+use RuntimeException;
+
+/**
+ * Class ValidationException.
+ */
+class ValidationException extends RuntimeException
+{
+    /**
+     * @var ValidationResult
+     */
+    private $validationResult;
+
+    /**
+     * ValidationException constructor.
+     *
+     * @param ValidationResult $validationResult
+     */
+    public function __construct(ValidationResult $validationResult)
+    {
+        parent::__construct($validationResult->getMessage());
+
+        $this->validationResult = $validationResult;
+    }
+
+    /**
+     * Get the validation result.
+     *
+     * @return ValidationResult
+     */
+    public function getValidationResult()
+    {
+        return $this->validationResult;
+    }
+}
