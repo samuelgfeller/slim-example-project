9bfa09f0bb7dd3d5abb3cfec82e94b7db7ba3492
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-03-12 16:30:42 +0100

    Moved login logic in UserService and passed object instead of array #2

--- src/Application/Controllers/Authentication/AuthController.php
+++ src/Application/Controllers/Authentication/AuthController.php
@@ -23,105 +23,82 @@ use Firebase\JWT\JWT;
 class AuthController extends Controller
 {
     protected $userService;
-    
+
     public function __construct(LoggerInterface $logger, UserService $userService)
     {
         parent::__construct($logger);
         $this->userService = $userService;
     }
-    
+
     public function register(Request $request, Response $response): Response
     {
         // If a html form name changes, these changes have to be done in the Entities constructor
         // too since these values will be the keys from the ArrayReader
         $userData = $request->getParsedBody();
-        
+
         $userData['password'] = password_hash($userData['password1'], PASSWORD_DEFAULT);
         // used to give login function
         $plainPass = $userData['password1'];
         unset($userData['password1'], $userData['password2']);
 
-        // Use Entity instead of DTO https://github.com/samuelgfeller/slim-api-example/issues/2#issuecomment-597245455
+        // Use Entity instead of DTO for simplicity https://github.com/samuelgfeller/slim-api-example/issues/2#issuecomment-597245455
         $user = new User(new ArrayReader($userData));
+
         try {
             $insertId = $this->userService->createUser($user);
         } catch (ValidationException $exception) {
             return $this->respondValidationError($exception->getValidationResult(), $response);
         }
-        
+
         // Log user in
         if (null !== $insertId) {
             $this->logger->info('User "' . $userData['email'] . '" created');
-            
+
             // Add email and password like it is expected in the login function
             $request = $request->withParsedBody(['email' => $userData['email'], 'password' => $plainPass]);
             // Call login function to authenticate the user
             // todo check if that is good practice or bad
             $loginResponse = $this->login($request, $response);
-            
+
             $loginContent = json_decode($loginResponse->getBody(), true);
-            
+
             // Clear response body after body content is saved
             $response = new \Slim\Psr7\Response();
-            
+
             $responseContent = $loginContent;
-            
+
             // maybe there is already a message so it has to be transformed as array
             $responseContent['message'] = [$loginContent['message']];
             $responseContent['message'][] = 'User created and logged in';
-            
+
             return $this->respondWithJson($response, $responseContent);
         }
         return $this->respondWithJson($response, ['status' => 'error', 'message' => 'User could not be registered']);
     }
-    
+
     public function login(Request $request, Response $response): Response
     {
         // todo add check if already logged in
-        
-        $parsedBody = $request->getParsedBody();
-        
-        $validationResult = $this->userValidation->validateUserLogin($parsedBody);
-        if ($validationResult->fails()) {
-            return $this->respondValidationError($validationResult, $response);
+
+        $userData = $request->getParsedBody();
+
+        $user = new User(new ArrayReader($userData));
+
+        try {
+            if ($userWithId = $this->userService->userAllowedToLogin($user)) {
+                $token = $this->userService->generateToken($userWithId);
+
+                return $this->respondWithJson(
+                    $response,
+                    ['token' => $token, 'status' => 'success', 'message' => 'Logged in'],
+                    200
+                );
+            }
+        } catch (ValidationException $exception) {
+            return $this->respondValidationError($exception->getValidationResult(), $response);
         }
-        
-        $loginData = $validationResult->getValidatedData();
-        
-        $user = $this->userService->findUserByEmail($loginData['email']);
-        //$this->logger->info('users/' . $user . ' has been called');
-        if ($user !== [] && password_verify($loginData['password'], $user['password'])) {
-            $durationInSec = 500; // In seconds
-            $tokenId = base64_encode(random_bytes(32));
-            $issuedAt = time();
-            $notBefore = $issuedAt + 2;             //Adding 2 seconds
-            $expire = $notBefore + $durationInSec;            // Adding 300 seconds
-            
-            $data = [
-                'iat' => $issuedAt,         // Issued at: time when the token was generated
-                'jti' => $tokenId,          // Json Token Id: an unique identifier for the token
-                'iss' => 'MyApp',       // Issuer
-                'nbf' => $notBefore,        // Not before
-                'exp' => $expire,           // Expire
-                'data' => [                  // Data related to the signer user
-                    'userId' => $user['id'], // userid from the users table
-                ]
-            ];
-            
-            $token = JWT::encode($data, 'test', 'HS256'); // todo change test to settings
-            
-            $this->logger->info(
-                'User "' . $loginData['email'] . '" logged in.
-                Token leased for ' . $durationInSec . 'sec'
-            );
-            
-            return $this->respondWithJson(
-                $response,
-                ['token' => $token, 'status' => 'success', 'message' => 'Logged in'],
-                200
-            );
-        }
-        $this->logger->notice('Invalid login attempt from "' . $loginData['email'] . '"');
+
+        $this->logger->notice('Invalid login attempt from "' . $user->getEmail() . '"');
         return $this->respondWithJson($response, ['status' => 'error', 'message' => 'Invalid credentials'], 401);
     }
 }
--- src/Domain/User/UserService.php
+++ src/Domain/User/UserService.php
@@ -4,18 +4,22 @@
 namespace App\Domain\User;
 
 use App\Infrastructure\Persistence\Exceptions\PersistenceRecordNotFoundException;
+use Firebase\JWT\JWT;
+use Psr\Log\LoggerInterface;
 
 class UserService
 {
     
-    private $userRepositoryInterface;
-    protected $userValidation;
+    private UserRepositoryInterface $userRepositoryInterface;
+    protected UserValidation $userValidation;
+    protected LoggerInterface $logger;
+
     
-    
-    public function __construct(UserRepositoryInterface $userRepositoryInterface, UserValidation $userValidation)
+    public function __construct(UserRepositoryInterface $userRepositoryInterface, UserValidation $userValidation,LoggerInterface $logger)
     {
         $this->userRepositoryInterface = $userRepositoryInterface;
         $this->userValidation = $userValidation;
+        $this->logger = $logger;
     }
     
     public function findAllUsers()
@@ -28,8 +32,12 @@ class UserService
     {
         return $this->userRepositoryInterface->findUserById($id);
     }
-    
-    public function findUserByEmail($email): array
+
+    /**
+     * @param string $email
+     * @return array|null
+     */
+    public function findUserByEmail(string $email):? array
     {
         return $this->userRepositoryInterface->findUserByEmail($email);
     }
@@ -46,7 +54,29 @@ class UserService
 //        $this->userValidation->validateUserRegistration($data);
         return $this->userRepositoryInterface->insertUser($user->toArray());
     }
-    
+
+    /**
+     * Checks if user is allowed to login.
+     * If yes, the user object is returned with id
+     * If no, null is returned
+     *
+     * @param User $user
+     * @return mixed|null
+     */
+    public function userAllowedToLogin(User $user)
+    {
+        // todo do validation
+//        $validationResult = $this->userValidation->validateUserLogin($parsedBody);
+
+        $dbUser = $this->findUserByEmail($user->getEmail());
+        //$this->logger->info('users/' . $user . ' has been called');
+        if($dbUser !== null && $dbUser !== [] && password_verify($user->getPassword(), $dbUser['password'])){
+            $user->setId($dbUser['id']);
+            return $user;
+        }
+        return null;
+    }
+
     /**
      * @param $id
      * @param $data array Data to update
@@ -73,6 +103,37 @@ class UserService
     public function deleteUser($id): bool
     {
         return $this->userRepositoryInterface->deleteUser($id);
+    }
+
+    /**
+     * Generates a JWT Token with user id
+     * todo move to jwt service
+     *
+     * @param User $user
+     * @return string
+     */
+    public function generateToken(User $user)
+    {
+        $durationInSec = 500; // In seconds
+        $tokenId = base64_encode(random_bytes(32));
+        $issuedAt = time();
+        $notBefore = $issuedAt + 2;             //Adding 2 seconds
+        $expire = $notBefore + $durationInSec;            // Adding 300 seconds
+
+        $data = [
+            'iat' => $issuedAt,         // Issued at: time when the token was generated
+            'jti' => $tokenId,          // Json Token Id: an unique identifier for the token
+            'iss' => 'MyApp',       // Issuer
+            'nbf' => $notBefore,        // Not before
+            'exp' => $expire,           // Expire
+            'data' => [                  // Data related to the signer user
+                'userId' => $user->getId(), // userid from the users table
+            ]
+        ];
+
+        return JWT::encode($data, 'test', 'HS256'); // todo change test to settings
+
+
     }
     
     /**
