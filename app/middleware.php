8a05bbac918d3b634221e0d251faa6eedaa40162
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-03-27 12:04:53 +0100

    Fix #11 cors exception on jwt error: moved jwt middleware before cors middleware

--- app/middleware.php
+++ app/middleware.php
@@ -8,29 +8,33 @@ use Slim\App;
 use Tuupola\Middleware\JwtAuthentication;
 
 return function (App $app) {
-
     $container = $app->getContainer();
 
     $settings = $container->get('settings');
     $logger = $container->get(LoggerInterface::class);
 
+    $app->add(
+        new JwtAuthentication(
+            [
+                //      'path' => '/api', /* or ["/api", "/admin"] */
+                'ignore' => ['/frontend', '/login', '/register', '/hello'],
+                'secret' => 'test',//$settings['settings']['jwt']['secret'],
+                'algorithm' => ['HS256'],
+                'logger' => $logger,
+                // HTTPS not mandatory for local development
+                'relaxed' => ['localhost', 'dev.slim-api-example', 'dev.frontend-example'],
+                'error' => function ($response, $arguments) {
+                    $data['status'] = 'error';
+                    $data['message'] = $arguments['message'];
+                    return $response->getBody()->write(
+                        json_encode($data, JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT, 512)
+                    );
+                }
+            ]
+        )
+    );
+
     $app->add(CorsMiddleware::class);
     $app->add(SessionMiddleware::class);
     $app->add(JsonBodyParserMiddleware::class);
-
-    $app->add(new JwtAuthentication([
-//      'path' => '/api', /* or ["/api", "/admin"] */
-        'ignore' => ['/frontend', '/login', '/register', '/hello'],
-        'secret' => 'test',//$settings['settings']['jwt']['secret'],
-        'algorithm' => ['HS256'],
-        'logger' => $logger,
-        // HTTPS not mandatory for local development
-        'relaxed' => ['localhost', 'dev.slim-api-example','dev.frontend-example'],
-        'error' => function ($response, $arguments) {
-            $data['status'] = 'error';
-            $data['message'] = $arguments['message'];
-            return $response->withHeader('Content-Type', 'application/json')->getBody()->write(json_encode($data,
-                    JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
-        }
-    ]));
 };
