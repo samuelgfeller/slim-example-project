a4095693f7f522e661685927344e0e197777b732
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-05-31 16:52:23 +0200

    Added bootstrap file

---
+++ app/bootstrap.php
@@ -0,0 +1,96 @@
+<?php
+
+// @todo remove that
+ini_set('xdebug.var_display_max_depth', '10');
+ini_set('xdebug.var_display_max_children', '256');
+ini_set('xdebug.var_display_max_data', '1024');
+
+// todo do I have to filter input data?
+// todo maybe put permission validation in a better place like UserValidation class?
+// todo testing junit
+// todo translation
+// todo https middleware https://odan.github.io/2020/04/07/slim4-https-middleware.html
+// todo use frontend framework
+
+
+use App\Application\Error\HtmlErrorRenderer;
+use Psr\Log\LoggerInterface;
+use Slim\Factory\AppFactory;
+use DI\ContainerBuilder;
+
+
+require __DIR__ . '/../vendor/autoload.php';
+
+/*
+ * Instantiate App
+ */
+// Instantiate PHP-DI ContainerBuilder
+$containerBuilder = new ContainerBuilder();
+
+// Set up settings
+$settings = require __DIR__ . '/../app/settings.php';
+$settings($containerBuilder);
+
+
+
+// Set up dependencies
+$dependencies = require __DIR__ . '/../app/dependencies.php';
+$dependencies($containerBuilder);
+
+// Set up repositories
+$repositories = require __DIR__ . '/../app/repositories.php';
+$repositories($containerBuilder);
+
+// Build PHP-DI Container instance
+$container = $containerBuilder->build();
+
+
+// Set container to create App with on AppFactory
+AppFactory::setContainer($container);
+// Instantiate the app
+$app = AppFactory::create();
+
+// Register middleware
+$middleware = require __DIR__ . '/../app/middleware.php';
+$middleware($app);
+
+
+/*$container->set('LocationController', function (ContainerInterface $c) {
+//    return new LocationController($c);
+});
+
+
+$container->set('LocationService', function (ContainerInterface $c) {
+    return new LocationService($c);
+});*/
+
+
+// testing cors http://www.test-cors.org/#?client_method=POST&client_credentials=false&server_url=http%3A%2F%2Fdev.slim_first_app%2Flocations&server_enable=true&server_status=200&server_credentials=false&server_tabs=remote
+// serv http://slimfirstapp.masesselin.ch/
+// doc https://www.slimframework.com/docs/v4/objects/routing.html#how-to-create-routes
+// Route group best practices: https://stackoverflow.com/questions/34502856/slim-3-framework-should-i-be-using-route-groups-for-my-api
+
+// Add Routing Middleware
+$app->addRoutingMiddleware();
+
+// Routing
+$routes = require __DIR__ . '/../app/routes.php';
+$routes($app);
+
+
+/*
+ * Add Error Handling Middleware
+ *
+ * @param bool $displayErrorDetails -> Should be set to false in production
+ * @param bool $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool $logErrorDetails -> Display error details in error log
+ * which can be replaced by a callable of your choice.
+
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
+ */
+$logger = $app->getContainer()->get(LoggerInterface::class);
+$errorMiddleware = $app->addErrorMiddleware(true, true, true, $logger);
+//$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorHandler = $errorMiddleware->getDefaultErrorHandler();
+//$errorHandler->registerErrorRenderer('text/html', HtmlErrorRenderer::class);
