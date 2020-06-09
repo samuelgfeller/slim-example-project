5d1bcc8741250e279c04c615960c4146da52428d
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-06-09 20:27:41 +0200

    Converted UserService tests to register mock in container and instantiate userService from container too #6

--- tests/Domain/User/UserServiceTest.php
+++ tests/Domain/User/UserServiceTest.php
@@ -2,74 +2,103 @@
 
 namespace App\Test\Domain\User;
 
+use App\Domain\User\User;
 use App\Domain\User\UserService;
 use App\Domain\User\UserValidation;
+use App\Domain\Utility\ArrayReader;
 use App\Infrastructure\User\UserRepository;
+use App\Test\UnitTestUtil;
 use Cake\Datasource\RepositoryInterface;
 use PHPUnit\Framework\TestCase;
 use Slim\Logger;
 
 class UserServiceTest extends TestCase
 {
-    private array $users;
+    use UnitTestUtil;
 
-
-    public function testFindAllUsers()
+    /**
+     * Test function findAllUsers from UserService
+     *
+     * @dataProvider \App\Test\Domain\User\UserProvider::oneSetOfMultipleUsersProvider()
+     * @param array $users
+     */
+    public function testFindAllUsers(array $users)
     {
-        $repoStub = $this->createMock(UserRepository::class);
-        $repoStub->method('findAllUsers')->willReturn($this->users);
+        // Mock the required repository and configure relevant method return value
+        $this->mock(UserRepository::class)
+            ->method('findAllUsers')
+            ->willReturn($users);
 
-        // Here I want to instantiate UserService with my custom repo stub but then I have to add the other dependencies as well
-        // They are only empty instances and they don't matter in this test case so it works but is that correct? What should / could I do else?
-        $service = new UserService($repoStub, new UserValidation(new Logger(), $repoStub), new Logger());
+        // Instantiate UserService where the autowire function used the previously defined custom mock
+        $service = $this->container->get(UserService::class);
 
-        $this->assertEquals($this->users, $service->findAllUsers());
-    //      Test passes
+        $this->assertEquals($users, $service->findAllUsers());
     }
 
-    public function testFindUser()
+    /**
+     * @dataProvider \App\Test\Domain\User\UserProvider::oneUserProvider()
+     * @param array $user
+     */
+    public function testFindUser(array $user)
     {
-        $user = $this->users[0];
+        // Mock the required repository and configure relevant method return value
+        $this->mock(UserRepository::class)
+            ->method('findUserById')
+            ->willReturn($user);
 
-        $repoStub = $this->createMock(UserRepository::class);
-        $repoStub->method('findUserById')->willReturn($user);
-
-        $service = new UserService($repoStub, new UserValidation(new Logger(), $repoStub), new Logger());
+        // Instantiate UserService where the autowire function used the previously defined custom mock
+        $service = $this->container->get(UserService::class);
 
         $this->assertEquals($user, $service->findUser($user['id']));
     }
 
-    public function testFindUserByEmail()
+    /**
+     * @dataProvider \App\Test\Domain\User\UserProvider::oneUserProvider()
+     * @param array $user
+     */
+    public function testFindUserByEmail(array $user)
     {
-        $user = $this->users[0];
+        // Mock the required repository and configure relevant method return value
+        $this->mock(UserRepository::class)
+            ->method('findUserByEmail')
+            ->willReturn($user);
 
-        $repoStub = $this->createMock(UserRepository::class);
-        $repoStub->method('findUserByEmail')->willReturn($user);
-
-        $service = new UserService($repoStub, new UserValidation(new Logger(), $repoStub), new Logger());
+        // Instantiate UserService where the autowire function used the previously defined custom mock
+        $service = $this->container->get(UserService::class);
 
         $this->assertEquals($user, $service->findUserByEmail($user['email']));
     }
 
-    public function testCreateUser()
+    /**
+     * @dataProvider \App\Test\Domain\User\UserProvider::oneUserProvider()
+     * @param array $validUser
+     */
+    public function testCreateUser(array $validUser)
     {
-        $validUser = $this->users[0];
+        // Mock the required repository and configure relevant method return value
+        $this->mock(UserRepository::class)
+            ->method('insertUser')
+            ->willReturn((string)$validUser['id']);
 
-        $repoStub = $this->createMock(UserRepository::class);
-        $repoStub->method('insertUser')->willReturn($validUser['id']);
+        // Instantiate UserService where the autowire function used the previously defined custom mock
+        /** @var UserService $service */
+        $service = $this->container->get(UserService::class);
 
-        $service = new UserService($repoStub, new UserValidation(new Logger(), $repoStub), new Logger());
+        // Create an user object
+        $userObj = new User(new ArrayReader($validUser));
 
-        $this->assertEquals($user, $service->findUserByEmail($user['email']));
+        // Return type of insertUser is string
+        $this->assertEquals($validUser['id'], $service->createUser($userObj));
     }
 
-    public function testUpdateUser()
-    {
-    }
+//    public function testUpdateUser()
+//    {
+//
+//    }
 
-    public function testDeleteUser()
-    {
-    }
+//    public function testDeleteUser()
+//    {
+//    }
 
 
 }
