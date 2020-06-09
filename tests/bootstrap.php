182de579dca4e89b306fca55eb41cbaab473ee6c
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-06-09 13:07:04 +0200

    Added phpunit.xml config and bootstrap in test folder #6

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,1 +1,11 @@
 <?php
+
+// Turn on output buffering - while output buffering is active no output is sent from the script (other than headers),
+// instead the output is stored in an internal buffer
+ob_start();
+
+if (!defined('APP_ENV')) {
+    define('APP_ENV', 'integration');
+}
+
+$app = require __DIR__ . '/../app/bootstrap.php';
