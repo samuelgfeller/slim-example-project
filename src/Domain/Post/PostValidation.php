86348938b8dede6ac22bda223fbede0336a4f81f
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-10-24 15:46:46 +0200

    Added user existence validation for post validation

--- src/Domain/Post/PostValidation.php
+++ src/Domain/Post/PostValidation.php
@@ -4,12 +4,28 @@ namespace App\Domain\Post;
 
 use App\Domain\Validation\AppValidation;
 use App\Domain\Validation\ValidationResult;
+use App\Infrastructure\User\UserRepository;
+use Psr\Log\LoggerInterface;
 
 /**
  * Class UserValidation
  */
 class PostValidation extends AppValidation
 {
+    /** @var UserRepository */
+    private UserRepository $userRepository;
+
+    /**
+     * UserValidation constructor.
+     *
+     * @param LoggerInterface $logger
+     * @param UserRepository $userRepository
+     */
+    public function __construct(LoggerInterface $logger, UserRepository $userRepository)
+    {
+        parent::__construct($logger);
+        $this->userRepository = $userRepository;
+    }
 
     /**
      * Validate post creation or update since they are the same
@@ -18,22 +34,44 @@ class PostValidation extends AppValidation
      */
     public function validatePostCreationOrUpdate(Post $post): void
     {
-        $validationResult = new ValidationResult('There is something in the post data which couldn\'t be validated');
+        $validationResult = new ValidationResult('There is something in the post data that couldn\'t be validated');
         // In case message gets validated in other function
         $required = true;
 
+        $postMsg = $post->getMessage();
+
         // Validate message
-        if (null !== $post->getMessage()) {
-            $this->validateLengthMax($post->getMessage(), 'message', $validationResult, 500);
-            $this->validateLengthMin($post->getMessage(), 'message', $validationResult, 4);
+        if (null !== $postMsg && '' !== $postMsg){
+            $this->validateLengthMax($postMsg, 'message', $validationResult, 500);
+            $this->validateLengthMin($postMsg, 'message', $validationResult, 4);
         } elseif (true === $required) {
             // If it is null but required, the user input is faulty so bad request 400 return status is sent
             $validationResult->setIsBadRequest(true, 'message', 'Message is required but not given');
         }
 
-        // todo does it make sense to check if user exists?
+        // Check if user exists
+        $this->validateUserExistence($post->getUserId(), $validationResult);
 
         $this->throwOnError($validationResult);
+    }
+
+    /**
+     * Check if user exists
+     * Same function than in UserValidation. Here again because as the functionalities
+     * grow, there could be other uses for the UserRepository. Maybe not though.
+     *
+     * @param $userId
+     * @param ValidationResult $validationResult
+     */
+    protected function validateUserExistence($userId, ValidationResult $validationResult): void
+    {
+        $exists = $this->userRepository->userExists($userId);
+        if (!$exists) {
+            $validationResult->setMessage('User not found');
+            $validationResult->setError('user', 'User not existing');
+
+            $this->logger->debug('Check for user (id: ' . $userId . ') that didn\'t exist in validation');
+        }
     }
 
 }
