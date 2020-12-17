c9d2d52b98c4e747283752d964c2eafdb3da76d2
Samuel Gfeller <samuelgfeller@bluewin.ch>
2020-12-17 14:07:33 +0100

    [SLE-64] added hello.html.php

---
+++ templates/hello/hello.html.php
@@ -0,0 +1,19 @@
+
+<!--https://samuel-gfeller.ch/favicon.ico-->
+<!--<img src="/assets/hello/slim-icon.png" alt="favicon">-->
+<pre></pre>
+<!--<ul>
+    <li><a href="{{ url_for('hello', { 'name': 'Josh' }) }}"
+           {% if is_current_url('hello', { 'name': 'Josh' }) %}class="active"{% endif %}>Josh</a></li>
+    <li><a href="{{ url_for('hello', { 'name': 'Samuel' }) }}"
+           {% if is_current_url('hello', { 'name': 'Samuel' }) %}class="active"{% endif %}>Samuel</a></li>
+    <li><a href="{{ url_for('hello', { 'name': 'Patrick' }) }}"
+           {% if is_current_url('hello', { 'name': 'patrick' }) %}class="active"{% endif %}>Patrick</a></li>
+</ul>
+-->
+<h1>Hello <?= $name ?>!</h1>
+<p>You have new notification(s).</p>
+<!--<a href="{{ url_for('login-page') }}">Login page</a>-->
+<?php
+$ini = ini_get('error_reporting');
+var_dump($ini) ?>
