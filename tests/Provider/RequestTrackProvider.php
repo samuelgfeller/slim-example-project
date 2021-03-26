29b8144df6ac99e1d532ff8714ceeb5b6dd7f411
Samuel Gfeller <samuelgfeller@bluewin.ch>
2021-03-26 18:33:08 +0100

    Tested separation from login and email security checks [SLE-118]

--- tests/Provider/RequestTrackProvider.php
+++ tests/Provider/RequestTrackProvider.php
@@ -64,24 +64,18 @@ class RequestTrackProvider
     }
 
     /**
-     * Provides all login request amounts in each different threshold where an exception must be thrown AND
-     * requests that exceed email sent limitation to avoid email abuse
-     *
-     * In the same provider to have only 1 test method
+     * Provides all login request amounts in each different threshold where an exception must be thrown
+     *  - Too many login failures in each threshold from same ip or specific user
+     *  - Too many login success requests (also for each threshold) from same ip or specific user
      *
      * @return array[]
      */
-    public function userLoginAndEmailProvider(): array
+    public function userLoginProvider(): array
     {
         // Values for logins (L)
         [$firstL, $secondL, $thirdL] = array_keys($this->userLoginThrottle);
         [$firstDelayL, $secondDelayL, $thirdDelayL] = array_values($this->userLoginThrottle);
-        // Values for emails (E)
-        [$firstE, $secondE, $thirdE] = array_keys($this->userEmailRequestThrottle);
-        [$firstDelayE, $secondDelayE, $thirdDelayE] = array_values($this->userEmailRequestThrottle);
 
-        // Example values as I can't take the values from settings.php because I can't access container in provider
-        // (Error: Typed property $container must not be accessed before initialization)
         return [
             // ! LOGIN FAILURE VALUES
             // ? First three are to test ip request stats
@@ -89,143 +83,121 @@ class RequestTrackProvider
             [
                 // request limit not needed as it's expected that error is thrown and that only happens if limit reached
                 'delay' => $firstDelayL,
-                'global_request_stats' => $this->stats(0, 'loginF'),
                 'ip_request_stats' => $this->stats($firstL, 'loginF'),
                 'user_request_stats' => $this->stats(0, 'loginF'),
-                'type' => SecurityException::USER_LOGIN,
             ],
             [
                 'delay' => $secondDelayL,
-                'global_request_stats' => $this->stats(0, 'loginF'),
                 'ip_request_stats' => $this->stats($secondL, 'loginF'),
                 'user_request_stats' => $this->stats(0, 'loginF'),
-                'type' => SecurityException::USER_LOGIN,
             ],
             [
                 'delay' => $thirdDelayL,
-                'global_request_stats' => $this->stats(0, 'loginF'),
                 'ip_request_stats' => $this->stats($thirdL, 'loginF'),
                 'user_request_stats' => $this->stats(0, 'loginF'),
-                'type' => SecurityException::USER_LOGIN,
             ],
 
             // ? Next are to test login requests made on one user
             [
                 'delay' => $firstDelayL,
-                'global_request_stats' => $this->stats(0, 'loginF'),
                 'ip_request_stats' => $this->stats(0, 'loginF'),
                 'user_request_stats' => $this->stats($firstL, 'loginF'),
-                'type' => SecurityException::USER_LOGIN,
             ],
             [
                 'delay' => $secondDelayL,
-                'global_request_stats' => $this->stats(0, 'loginF'),
                 'ip_request_stats' => $this->stats(0, 'loginF'),
                 'user_request_stats' => $this->stats($secondL, 'loginF'),
-                'type' => SecurityException::USER_LOGIN,
             ],
             [
                 'delay' => $thirdDelayL,
-                'global_request_stats' => $this->stats(0, 'loginF'),
                 'ip_request_stats' => $this->stats(0, 'loginF'),
                 'user_request_stats' => $this->stats($thirdL, 'loginF'),
-                'type' => SecurityException::USER_LOGIN,
             ],
             // ! LOGIN SUCCESS VALUES
             // ? First three are to test ip request stats
             [
                 // request limit not needed as it's expected that error is thrown and that only happens if limit reached
                 'delay' => $firstDelayL,
-                'global_request_stats' => $this->stats(0, 'loginS'),
                 'ip_request_stats' => $this->stats($firstL, 'loginS'),
                 'user_request_stats' => $this->stats(0, 'loginS'),
-                'type' => SecurityException::USER_LOGIN,
             ],
             [
                 'delay' => $secondDelayL,
-                'global_request_stats' => $this->stats(0, 'loginS'),
                 'ip_request_stats' => $this->stats($secondL, 'loginS'),
                 'user_request_stats' => $this->stats(0, 'loginS'),
-                'type' => SecurityException::USER_LOGIN,
             ],
             [
                 'delay' => $thirdDelayL,
-                'global_request_stats' => $this->stats(0, 'loginS'),
                 'ip_request_stats' => $this->stats($thirdL, 'loginS'),
                 'user_request_stats' => $this->stats(0, 'loginS'),
-                'type' => SecurityException::USER_LOGIN,
             ],
 
             // ? Next are to test login requests made on one user
             [
                 'delay' => $firstDelayL,
-                'global_request_stats' => $this->stats(0, 'loginS'),
                 'ip_request_stats' => $this->stats(0, 'loginS'),
                 'user_request_stats' => $this->stats($firstL, 'loginS'),
-                'type' => SecurityException::USER_LOGIN,
             ],
             [
                 'delay' => $secondDelayL,
-                'global_request_stats' => $this->stats(0, 'loginS'),
                 'ip_request_stats' => $this->stats(0, 'loginS'),
                 'user_request_stats' => $this->stats($secondL, 'loginS'),
-                'type' => SecurityException::USER_LOGIN,
             ],
             [
                 'delay' => $thirdDelayL,
-                'global_request_stats' => $this->stats(0, 'loginS'),
                 'ip_request_stats' => $this->stats(0, 'loginS'),
                 'user_request_stats' => $this->stats($thirdL, 'loginS'),
-                'type' => SecurityException::USER_LOGIN,
             ],
+        ];
+    }
 
-            // ! EMAIL VALUES
-            // ? First three are to test ip request stats
-            // All thresholds for email requests coming from the same ip. Throttled same as rapid fire on user.
+    /**
+     * Provides values for email abuse test concerning specific email or coming from ip.
+     * Content are requests that exceed each email send limitation.
+     *
+     * @return array[]
+     */
+    public function userEmailProvider()
+    {
+        // Values for emails (E)
+        [$firstE, $secondE, $thirdE] = array_keys($this->userEmailRequestThrottle);
+        [$firstDelayE, $secondDelayE, $thirdDelayE] = array_values($this->userEmailRequestThrottle);
+
+        // ? First three are to test ip request stats
+        // All thresholds for email requests coming from the same ip. Throttled same as rapid fire on user.
+        return [
             [
                 // request limit not needed as it's expected that error is thrown and that only happens if limit reached
                 'delay' => $firstDelayE,
-                'global_request_stats' => $this->stats(0, 'email'),
                 'ip_request_stats' => $this->stats($firstE, 'email'),
                 'user_request_stats' => $this->stats(0, 'email'),
-                'type' => SecurityException::USER_EMAIL,
             ],
             [
                 'delay' => $secondDelayE,
-                'global_request_stats' => $this->stats(0, 'email'),
                 'ip_request_stats' => $this->stats($secondE, 'email'),
                 'user_request_stats' => $this->stats(0, 'email'),
-                'type' => SecurityException::USER_EMAIL,
             ],
             [
                 'delay' => $thirdDelayE,
-                'global_request_stats' => $this->stats(0, 'email'),
                 'ip_request_stats' => $this->stats($thirdE, 'email'),
                 'user_request_stats' => $this->stats(0, 'email'),
-                'type' => SecurityException::USER_EMAIL,
             ],
 
             // ? Next are to test email requests made on one user
             [
                 'delay' => $firstDelayE,
-                'global_request_stats' => $this->stats(0, 'email'),
                 'ip_request_stats' => $this->stats(0, 'email'),
                 'user_request_stats' => $this->stats($firstE, 'email'),
-                'type' => SecurityException::USER_EMAIL,
             ],
             [
                 'delay' => $secondDelayE,
-                'global_request_stats' => $this->stats(0, 'email'),
                 'ip_request_stats' => $this->stats(0, 'email'),
                 'user_request_stats' => $this->stats($secondE, 'email'),
-                'type' => SecurityException::USER_EMAIL,
             ],
             [
                 'delay' => $thirdDelayE,
-                'global_request_stats' => $this->stats(0, 'email'),
                 'ip_request_stats' => $this->stats(0, 'email'),
                 'user_request_stats' => $this->stats($thirdE, 'email'),
-                'type' => SecurityException::USER_EMAIL,
             ],
         ];
     }
--- tests/Unit/Domain/Security/SecurityServiceTest.php
+++ tests/Unit/Domain/Security/SecurityServiceTest.php
@@ -9,136 +9,106 @@ use App\Test\AppTestTrait;
 use PHPUnit\Framework\TestCase;
 use Selective\TestTrait\Traits\DatabaseTestTrait;
 
+/**
+ * Threats:
+ *  - Rapid fire attacks (when bots try to log in with 1000 different passwords on one user account)
+ *  - Email abuse (sending a lot of emails may be costly)
+ *  - Distributed brute force attacks (try to log in 1000 different users with most common password)
+ *
+ * Testing whole function performLoginSecurityCheck() and performEmailAbuseCheck() and not sub-functions directly as
+ * they are private mainly because here (https://stackoverflow.com/a/2798203/9013718 comments), they say:
+ * > You should not test protected/private members directly. They belong to the internal implementation of the class,
+ * > and should not be coupled with the test. This makes refactoring impossible and eventually you don't test what
+ * > needs to be tested. You need to test them indirectly using public methods.
+ * I thought it would make sense to test each function separately to avoid the following complex test function and
+ * I don't want those sub-functions to be public as the security check is always done in its entirety from outside.
+ * But probably there are things I'm missing out on and it seems that the internet agrees that it's a bad practice.
+ */
 class SecurityServiceTest extends TestCase
 {
     use AppTestTrait;
-    use DatabaseTestTrait;
 
     /**
-     * Threats:
-     *  - Rapid fire attacks (when bots try to log in with 1000 different passwords on one user account)
-     *  - Email abuse (sending a lot of emails may be costly for hoster)
      * Covered in this test:
-     *  - [Login from ip] Test with every defined threshold of login failure and success requests coming from the same
+     * - [Login from ip] Test with every defined threshold of login failure and success requests coming from the same
      *    ip. Throttled same as rapid fire
-     *  - [Login with user] Test with every defined (in provider) threshold of login failure and success requests
+     * - [Login with user] Test with every defined (in provider) threshold of login failure and success requests
      *    concerning the same user (target email)
-     *  - [Individual Email abuse] Test with every defined threshold of requests sending an email from a specific ip or
-     *    concerning an email
-     *
-     * Behaviour: Throttle log in attempts per user
-     *  - After x amount of login requests or sent emails in an hour, user has to wait a certain delay before trying again
-     *  - For each next login request or next email sent in the same hour, the user has to wait the same delay
-     *  - Until it eventually increases after value y
-     *  - If login or email requests continue, at amount z captcha is required from the user
-     *  - This rule applies to login requests on a specific user or login requests coming from a specific ip
-     *
-     * Testing whole function performSecurityCheck() and not sub-functions directly as they are private mainly because
-     * here (https://stackoverflow.com/a/2798203/9013718 comments), they say.
-     * > You should not test protected/private members directly. They belong to the internal implementation of the class,
-     * > and should not be coupled with the test. This makes refactoring impossible and eventually you don't test what
-     * > needs to be tested. You need to test them indirectly using public methods.
-     * I thought it would make sense to test each function separately to avoid the following complex test function and
-     * I don't want those sub-functions to be public as the security check is always done in its entirety from outside.
-     * But probably there are things I'm missing out on and it seems that the internet agrees that it's a bad practice.
      *
      * Data provider is very important in this test. It will call this function with all the different kinds of user
-     * request amounts where an exception must be thrown. This is a generic test function, to change the tested
-     * content, the provider should be adapted.
-     * @dataProvider \App\Test\Provider\RequestTrackProvider::userLoginAndEmailProvider()
+     * request amounts where an exception must be thrown.
+     * @dataProvider \App\Test\Provider\RequestTrackProvider::userLoginProvider()
      *
-     * request limit not needed as it's expected that error is thrown and that only happens if limit reached
-     * @param int|string $delay delay in seconds or 'captcha'
-     * @param array $globalRequestStats has to be included in provider as globalRequestsStats have to be at least as low
-     * as the same value of user stats. It wouldn't make sense otherwise.
-     * @param array $ipRequestStats array with stats (request_amount, sent_emails, login_failures, login_successes)
-     * @param array $userRequestStats array with stats (request_amount, sent_emails, login_failures, login_successes)
-     * @param string $testType USER_LOGIN|USER_EMAIL if provided data is for an email abuse test or login test
+     * @param int|string $delay
+     * @param array $ipRequestStats
+     * @param array $userRequestStats
      */
-    public function testPerformSecurityCheck_userRapidFireAndEmailAbuse(
+    public function testPerformLoginSecurityCheck_user(
         int|string $delay,
-        array $globalRequestStats,
         array $ipRequestStats,
-        array $userRequestStats,
-        string $testType
+        array $userRequestStats
     ): void {
-        // Preparation; making sure other security checks won't fail
         $requestTrackRepository = $this->mock(RequestTrackRepository::class);
-        // Mock with 0 stats to prevent undefined index notice
-        $requestTrackRepository->method('getGlobalRequestStats')->willReturn($globalRequestStats);
+
         // Very important to return stats otherwise global check fails
-        $requestTrackRepository->method('getLoginAmountStats')->willReturn(
+        $requestTrackRepository->method('getGlobalLoginAmountStats')->willReturn(
             ['login_total' => 10, 'login_failures' => 0] // 0 percent failures so global check won't fail
         );
-        $requestTrackRepository->method('getGlobalSentEmailAmount')->willReturn('0');
 
         // Actual test
-        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
         // Provider first makes $ipRequestStats filled with each values exceeding threshold (new threshold on each run)
         $requestTrackRepository->method('getIpRequestStats')->willReturn($ipRequestStats);
         // Vice versa $userRequestStats are 0 values when ip values are tested but full later for user tests
         $requestTrackRepository->method('getUserRequestStats')->willReturn($userRequestStats);
 
+        // lastRequest has to be defined here. In the provider "created_at" seconds often differs from assertion
         $lastRequest = [
             'id' => 12,
             'email' => 'email.does@not-matter.com',
             'ip_address' => 2130706433, // 127.0.0.1 as unsigned int
             'sent_email' => 1,
-            'is_login' => 'success', // Not relevant for login test
+            'is_login' => 'success', // Not relevant for individual login and email test
             'created_at' => date('Y-m-d H:i:s'), // Current time so delay will be the original length
         ];
-        if ($testType === SecurityException::USER_LOGIN) {
-            // Relevant for login tests
-            $requestTrackRepository->method('findLatestLoginRequestFromUserOrIp')->willReturn($lastRequest);
-            $this->expectExceptionMessage('Exceeded maximum of tolerated login requests.');
-            $exceptionType = SecurityException::USER_LOGIN;
-        } elseif ($testType === SecurityException::USER_EMAIL) {
-            // Relevant for email abuse tests
-            $requestTrackRepository->method('findLatestEmailRequestFromUserOrIp')->willReturn($lastRequest);
-            $this->expectExceptionMessage('Exceeded maximum of tolerated emails.');
-            $exceptionType = SecurityException::USER_EMAIL;
-        } else {
-            self::fail('$testType must be either "user_email" or "user_login"');
-        }
+        $requestTrackRepository->method('findLatestLoginRequestFromUserOrIp')->willReturn($lastRequest);
 
         /** @var SecurityService $securityService */
         $securityService = $this->container->get(SecurityService::class);
 
+        // Assert
         $this->expectException(SecurityException::class);
+        $this->expectExceptionMessage('Exceeded maximum of tolerated login requests.');
+
         // In try catch to assert exception attributes
         try {
-            $securityService->performSecurityCheck('email.does@not-matter.com');
+            $securityService->performLoginSecurityCheck('email.does@not-matter.com');
         } catch (SecurityException $se) {
-            self::assertSame($exceptionType, $se->getType());
+            self::assertSame(SecurityException::USER_LOGIN, $se->getType());
             $delayMessage = 'Remaining delay not matching. ' .
                 'May be because mock created_at time and assertion were done in different seconds so please try again';
             self::assertSame($delay, $se->getRemainingDelay(), $delayMessage);
             // Throw because it's expected to verify that exception is thrown
             throw $se;
         }
-        // Not so ideal test case as it contains quite complex code; it's almost as test function needs its own unit test
     }
-
 
     /**
      * Threat: Distributed brute force attacks (try to log in 1000 different users with most common password)
+     *
      * Covered in this test:
      *  - Global login failures exceeding allowed threshold
      */
-    public function testPerformSecurityCheck_globalLogin(): void
+    public function testPerformLoginSecurityCheck_global(): void
     {
-        // Preparation; making sure other security checks won't fail
         $requestTrackRepository = $this->mock(RequestTrackRepository::class);
+
+        // Preparation; making sure other security checks won't fail
         // User stats should be 0 as global is tested here
         $emptyStats = ['request_amount' => 0, 'sent_emails' => 0, 'login_failures' => 0, 'login_successes' => 0];
         $requestTrackRepository->method('getIpRequestStats')->willReturn($emptyStats);
         $requestTrackRepository->method('getUserRequestStats')->willReturn($emptyStats);
-        // Since Login is tested here it has to be made sure that email abuse check doesn't fail
-        $requestTrackRepository->method('getGlobalSentEmailAmount')->willReturn('0');
-        // Not needed to mock getGlobalStats as it's not used for the login check
 
         // Actual test starts here
-        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
         // Login amount stats used to calculate threshold
         $totalLogins = 1000; // This amount doesn't matter (could be other int); the later calculated ratio does
         $loginAmountStats = [
@@ -147,7 +117,7 @@ class SecurityServiceTest extends TestCase
             'login_failures' => $totalLogins / 100 *
                 $this->container->get('settings')['security']['login_failure_percentage']
         ];
-        $requestTrackRepository->method('getLoginAmountStats')->willReturn($loginAmountStats);
+        $requestTrackRepository->method('getGlobalLoginAmountStats')->willReturn($loginAmountStats);
 
         /** @var SecurityService $securityService */
         $securityService = $this->container->get(SecurityService::class);
@@ -158,10 +128,70 @@ class SecurityServiceTest extends TestCase
 
         // In try catch to assert exception attributes
         try {
-            $securityService->performSecurityCheck('email.does@not-matter.com');
+            $securityService->performLoginSecurityCheck('email.does@not-matter.com');
         } catch (SecurityException $se) {
             self::assertSame(SecurityException::GLOBAL_LOGIN, $se->getType());
             self::assertSame('captcha', $se->getRemainingDelay());
+            // Throw because it's expected to verify that exception is thrown
+            throw $se;
+        }
+    }
+
+    /**
+     * Covered in this test:
+     *  - [Individual Email abuse] Test with every defined threshold of requests sending an email from a specific ip or
+     *    concerning an email
+     *
+     * Data provider is very important in this test. It will call this function with all the different kinds of user
+     * request amounts where an exception must be thrown.
+     * @dataProvider \App\Test\Provider\RequestTrackProvider::userEmailProvider()
+     *
+     * @param int|string $delay
+     * @param array $ipRequestStats
+     * @param array $userRequestStats
+     */
+    public function testPerformEmailAbuseCheck_user(
+        int|string $delay,
+        array $ipRequestStats,
+        array $userRequestStats
+    ): void {
+        // Preparation; making sure other security checks won't fail
+        $requestTrackRepository = $this->mock(RequestTrackRepository::class);
+        // Very important to return stats otherwise global check fails
+        $requestTrackRepository->method('getGlobalSentEmailAmount')->willReturn('0');
+
+        // Actual test
+        // Provider first makes $ipRequestStats filled with each values exceeding threshold (new threshold on each run)
+        $requestTrackRepository->method('getIpRequestStats')->willReturn($ipRequestStats);
+        // Vice versa $userRequestStats are 0 values when ip values are tested but full later for user tests
+        $requestTrackRepository->method('getUserRequestStats')->willReturn($userRequestStats);
+
+        // lastRequest has to be defined here. In the provider "created_at" seconds often differs from assertion
+        $lastRequest = [
+            'id' => 12,
+            'email' => 'email.does@not-matter.com',
+            'ip_address' => 2130706433, // 127.0.0.1 as unsigned int
+            'sent_email' => 1,
+            'is_login' => 'success', // Not relevant for login test
+            'created_at' => date('Y-m-d H:i:s'), // Current time so delay will be the original length
+        ];
+        // Relevant for email abuse tests
+        $requestTrackRepository->method('findLatestEmailRequestFromUserOrIp')->willReturn($lastRequest);
+
+        /** @var SecurityService $securityService */
+        $securityService = $this->container->get(SecurityService::class);
+
+        // Assertions
+        $this->expectException(SecurityException::class);
+        $this->expectExceptionMessage('Exceeded maximum of tolerated emails.');
+        // In try catch to assert exception attributes
+        try {
+            $securityService->performEmailAbuseCheck('email.does@not-matter.com');
+        } catch (SecurityException $se) {
+            self::assertSame(SecurityException::USER_EMAIL, $se->getType());
+            $delayMessage = 'Remaining delay not matching. ' .
+                'May be because mock created_at time and assertion were done in different seconds so please try again';
+            self::assertSame($delay, $se->getRemainingDelay(), $delayMessage);
             // Throw because it's expected to verify that exception is thrown
             throw $se;
         }
@@ -179,24 +209,24 @@ class SecurityServiceTest extends TestCase
      * @param string $dailyEmailAmount too many daily emails
      * @param string $monthlyEmailAmount too many monthly emails
      */
-    public function testPerformSecurityCheck_globalEmailAbuse(string $dailyEmailAmount, string $monthlyEmailAmount): void {
-        // Preparation; making sure other security checks won't fail
-        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+    public function testPerformSecurityCheck_globalEmailAbuse(
+        string $dailyEmailAmount,
+        string $monthlyEmailAmount
+    ): void {
         $requestTrackRepository = $this->mock(RequestTrackRepository::class);
+
+        // Preparation; making sure other security checks won't fail
         // User stats should be 0 as global is tested here
         $emptyStats = ['request_amount' => 0, 'sent_emails' => 0, 'login_failures' => 0, 'login_successes' => 0];
         $requestTrackRepository->method('getIpRequestStats')->willReturn($emptyStats);
         $requestTrackRepository->method('getUserRequestStats')->willReturn($emptyStats);
-        // Since email abuse is tested here it has to be made sure that global login check doesn't fail
-        $requestTrackRepository->method('getLoginAmountStats')->willReturn(
-            ['login_total' => 0, 'login_failures' => 0]
-        );
 
         // Actual test starts here
         // In the first test iteration the provider sets the daily amount and leaves monthly blank
         // The second time this test is executed the provider sets monthly amount and leaves daily blank
         $requestTrackRepository->method('getGlobalSentEmailAmount')->willReturnOnConsecutiveCalls(
-            $dailyEmailAmount, $monthlyEmailAmount
+            $dailyEmailAmount,
+            $monthlyEmailAmount
         );
 
         /** @var SecurityService $securityService */
@@ -206,10 +236,10 @@ class SecurityServiceTest extends TestCase
         $this->expectException(SecurityException::class);
         // For the daily amount test, $monthlyEmailAmount is the same as daily. If its more it means that this test
         // iteration is about monthly amount
-        if ($monthlyEmailAmount > $dailyEmailAmount){
+        if ($monthlyEmailAmount > $dailyEmailAmount) {
             $this->expectExceptionMessage('Maximum amount of unrestricted email sending monthly reached site-wide.');
         } // The least possible monthly values is the same as daily which is given by the provider for the daily test
-        elseif($monthlyEmailAmount === $dailyEmailAmount) {
+        elseif ($monthlyEmailAmount === $dailyEmailAmount) {
             $this->expectExceptionMessage('Maximum amount of unrestricted email sending daily reached site-wide.');
         } else {
             self::fail('Monthly email expected to be either greater than daily or the same');
@@ -217,7 +247,7 @@ class SecurityServiceTest extends TestCase
 
         // In try catch to assert exception attributes
         try {
-            $securityService->performSecurityCheck('email.does@not-matter.com');
+            $securityService->performEmailAbuseCheck('email.does@not-matter.com');
         } catch (SecurityException $se) {
             self::assertSame(SecurityException::GLOBAL_EMAIL, $se->getType());
             self::assertSame('captcha', $se->getRemainingDelay());
