7f11f5ea210bccf24188b6e9c632cc1e2eb4b8a1
Samuel Gfeller <samuelgfeller@bluewin.ch>
2022-03-29 18:01:40 +0200

    Added working async password strength checker [SLE-155]

--- public/assets/auth/password-strength-checker.js
+++ public/assets/auth/password-strength-checker.js
@@ -1,3 +1,4 @@
+// Init vars
 let password1Inp = document.getElementById('password1-inp')
 let password2Inp = document.getElementById('password2-inp');
 
@@ -6,37 +7,38 @@ password1Inp.addEventListener('keyup', checkIfPasswordsMatch);
 password2Inp.addEventListener('keyup', checkIfPasswordsMatch);
 
 // Check if password is known to be breached
-password1Inp.addEventListener('change', checkPasswordStrength);
+password1Inp.addEventListener('change', checkIfPasswordIsBreached);
 
-
+/**
+ * Check if password 1 and password 2 are identical
+ */
 function checkIfPasswordsMatch() {
     let submitBtn = document.querySelector('input[type="submit"]');
     // Set button to disabled if passwords don't match or if field is empty
     submitBtn.disabled = password1Inp.value !== password2Inp.value || password1Inp.value === '';
 }
 
-function checkPasswordStrength() {
-    if (isBreached(password1Inp.value)) {
-        if (document.getElementById('pwned-password-warning').length === 0) {
-            password1Inp.after('<span class="input-warning" id="pwned-password-warning">' +
-                'This password is known to have been leaked and is unsafe to use</span>');
-        }
-    } else {
-        document.getElementById('pwned-password-warning').remove();
-    }
+/**
+ * Check if password has been breached
+ */
+function checkIfPasswordIsBreached() {
+    // Create hash and make Ajax request to HIBP api and display warning if needed
+    getHash(password1Inp.value)
+        // passwordHash is the return value of the promise above
+        .then(passwordHash => {
+            // console.log(passwordHash);
+            makeHIBPRequest(passwordHash)
+        });
 }
 
 /**
- * Makes request to haveibeenpwned
+ * Make request to Have I Been Pwned API
  *
- * @param password
- * @returns {boolean}
+ * @param {string} passwordHash
  */
-function isBreached(password) {
-    let pwHash = sha1(password);
-    let hashPrefix = pwHash.substring(0, 5);
-    let hashSuffix = pwHash.substring(5);
-    let result = false;
+function makeHIBPRequest(passwordHash) {
+    let hashPrefix = passwordHash.substring(0, 5);
+    let hashSuffix = passwordHash.substring(5);
 
     // todo implement async function to return after result https://stackoverflow.com/a/5316805/9013718
     let xHttp = new XMLHttpRequest();
@@ -46,146 +48,73 @@ function isBreached(password) {
             if (xHttp.status !== 200) {
                 // Default fail handler
                 handleFail(xHttp);
+                removeWarning();
             }
 
             // Success
             else {
-                result = xHttp.responseText.toLowerCase().includes(hashSuffix);
+                let hashFound = xHttp.responseText.toLowerCase().includes(hashSuffix);
+                if (hashFound === true) {
+                    // Callback function
+                    showWarning();
+                } else {
+                    removeWarning();
+                }
             }
         }
     };
     // For GET requests, query params have to be passed in the url directly. They are ignored in send()
     xHttp.open('GET', `https://api.pwnedpasswords.com/range/${hashPrefix}`, false);
-    xHttp.setRequestHeader("Content-type", "application/json");
 
     xHttp.send();
-
-    return result;
 }
 
 /**
- * Creates sha1 hash of arbitrary string
- *
- * @param msg
- * @returns {string}
+ * Add warning for the user below input field
  */
-function sha1(msg) {
-    function rotate_left(n, s) {
-        return (n << s) | (n >>> (32 - s));
+function showWarning() {
+    if (null === document.getElementById('pwned-password-warning')) {
+        password1Inp.insertAdjacentHTML('afterend', '<span class="input-warning" id="pwned-password-warning">' +
+            'This password is known to have been leaked and is unsafe to use</span>');
     }
-    function cvt_hex(val) {
-        var str = '';
-        var i;
-        var v;
-        for (i = 7; i >= 0; i--) {
-            v = (val >>> (i * 4)) & 0x0f;
-            str += v.toString(16);
-        }
-        return str;
-    }
+}
 
-    function Utf8Encode(string) {
-        string = string.replace(/\r\n/g, '\n');
-        var utftext = '';
-        for (var n = 0; n < string.length; n++) {
-            var c = string.charCodeAt(n);
-            if (c < 128) {
-                utftext += String.fromCharCode(c);
-            } else if ((c > 127) && (c < 2048)) {
-                utftext += String.fromCharCode((c >> 6) | 192);
-                utftext += String.fromCharCode((c & 63) | 128);
-            } else {
-                utftext += String.fromCharCode((c >> 12) | 224);
-                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-                utftext += String.fromCharCode((c & 63) | 128);
+/**
+ * Remove warning below input field
+ */
+function removeWarning() {
+    // If not breached, remove warning element if it exists
+    let warningElement = document.getElementById('pwned-password-warning');
+    if (null !== warningElement) {
+        warningElement.remove();
+    }
+}
+
+/**
+ * Create SHA-1 hash
+ *
+ * Source: https://stackoverflow.com/a/43383990/9013718
+ * @param str
+ * @param algo
+ * @returns {Promise<string>}
+ */
+function getHash(str, algo = "SHA-1") {
+    let strBuf = new TextEncoder().encode(str);
+    // digest returns a promise
+    return crypto.subtle.digest(algo, strBuf)
+        // .then is executed only after initial promise is done (resolved) and accepts two parameters
+        // first is the success callback function and second the error callback function
+        // hash is the variable name of the return result of the promise before (resolved value)
+        // which is passed to an anonymous function that can use this value (hashAsParam) => {/* function */}
+        .then(hash => {
+            window.hash = hash;
+            // here hash is an arrayBuffer,
+            // So we'll convert it to its hex version
+            let result = '';
+            const view = new DataView(hash);
+            for (let i = 0; i < hash.byteLength; i += 4) {
+                result += ('00000000' + view.getUint32(i).toString(16)).slice(-8);
             }
-        }
-        return utftext;
-    }
-
-    var blockstart;
-    var i, j;
-    var W = new Array(80);
-    var H0 = 0x67452301;
-    var H1 = 0xEFCDAB89;
-    var H2 = 0x98BADCFE;
-    var H3 = 0x10325476;
-    var H4 = 0xC3D2E1F0;
-    var A, B, C, D, E;
-    var temp;
-    msg = Utf8Encode(msg);
-    var msg_len = msg.length;
-    var word_array = [];
-    for (i = 0; i < msg_len - 3; i += 4) {
-        j = msg.charCodeAt(i) << 24 | msg.charCodeAt(i + 1) << 16 |
-            msg.charCodeAt(i + 2) << 8 | msg.charCodeAt(i + 3);
-        word_array.push(j);
-    }
-    switch (msg_len % 4) {
-        case 0:
-            i = 0x080000000;
-            break;
-        case 1:
-            i = msg.charCodeAt(msg_len - 1) << 24 | 0x0800000;
-            break;
-        case 2:
-            i = msg.charCodeAt(msg_len - 2) << 24 | msg.charCodeAt(msg_len - 1) << 16 | 0x08000;
-            break;
-        case 3:
-            i = msg.charCodeAt(msg_len - 3) << 24 | msg.charCodeAt(msg_len - 2) << 16 | msg.charCodeAt(msg_len - 1) << 8 | 0x80;
-            break;
-    }
-    word_array.push(i);
-    while ((word_array.length % 16) != 14) word_array.push(0);
-    word_array.push(msg_len >>> 29);
-    word_array.push((msg_len << 3) & 0x0ffffffff);
-    for (blockstart = 0; blockstart < word_array.length; blockstart += 16) {
-        for (i = 0; i < 16; i++) W[i] = word_array[blockstart + i];
-        for (i = 16; i <= 79; i++) W[i] = rotate_left(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
-        A = H0;
-        B = H1;
-        C = H2;
-        D = H3;
-        E = H4;
-        for (i = 0; i <= 19; i++) {
-            temp = (rotate_left(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
-            E = D;
-            D = C;
-            C = rotate_left(B, 30);
-            B = A;
-            A = temp;
-        }
-        for (i = 20; i <= 39; i++) {
-            temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
-            E = D;
-            D = C;
-            C = rotate_left(B, 30);
-            B = A;
-            A = temp;
-        }
-        for (i = 40; i <= 59; i++) {
-            temp = (rotate_left(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
-            E = D;
-            D = C;
-            C = rotate_left(B, 30);
-            B = A;
-            A = temp;
-        }
-        for (i = 60; i <= 79; i++) {
-            temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
-            E = D;
-            D = C;
-            C = rotate_left(B, 30);
-            B = A;
-            A = temp;
-        }
-        H0 = (H0 + A) & 0x0ffffffff;
-        H1 = (H1 + B) & 0x0ffffffff;
-        H2 = (H2 + C) & 0x0ffffffff;
-        H3 = (H3 + D) & 0x0ffffffff;
-        H4 = (H4 + E) & 0x0ffffffff;
-    }
-    var temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
-
-    return temp.toLowerCase();
+            return result;
+        });
 }
