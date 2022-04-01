53f6b457b133c48d39a7fffcbed3153e9fbf0a20
Samuel Gfeller <samuelgfeller@bluewin.ch>
2022-04-01 14:12:59 +0200

    Added DatabaseExtensionTestTrait [selective-php/test-traits#4][SLE-71]

---
+++ tests/Traits/DatabaseExtensionTestTrait.php
@@ -0,0 +1,80 @@
+<?php
+
+
+namespace App\Test\Traits;
+
+
+use PDO;
+use Selective\TestTrait\Traits\DatabaseTestTrait;
+
+/**
+ * Slim App Route Test Trait.
+ */
+trait DatabaseExtensionTestTrait
+{
+    use DatabaseTestTrait;
+
+    /**
+     * Fetch rows by given column.
+     *
+     * @param string $table Table name
+     * @param string $whereColumn The column name of the select query
+     * @param mixed $whereValue The value that will be searched for
+     * @param array|null $fields The array of fields
+     *
+     * @return array[] array or rows
+     */
+    protected function findTableRowsByColumn(
+        string $table,
+        string $whereColumn,
+        mixed $whereValue,
+        array $fields = null
+    ): array {
+        $sql = sprintf('SELECT * FROM `%s` WHERE `%s` = :whereValue', $table, $whereColumn);
+        $statement = $this->createPreparedStatement($sql);
+        $statement->execute(['whereValue' => $whereValue]);
+
+        $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        $rowsWithFilteredFields = null;
+        if ($fields) {
+            foreach ($rows as $row) {
+                $rowsWithFilteredFields[] = array_intersect_key($row, array_flip($fields));
+            }
+        }
+
+        return $rowsWithFilteredFields ?? $rows;
+    }
+
+    /**
+     * Asserts that results of given table are the same as the given row.
+     *
+     * @param array $expectedRow Row expected to find
+     * @param string $table Table to look into
+     * @param string $whereColumn The column of the search query
+     * @param mixed $whereValue The value that will be searched for
+     * @param array|null $fields The array of fields
+     * @param string $message Optional message
+     *
+     * @return void
+     */
+    protected function assertTableRowsByColumn(
+        array $expectedRow,
+        string $table,
+        string $whereColumn,
+        mixed $whereValue,
+        array $fields = null,
+        string $message = ''
+    ): void {
+        $rows = $this->findTableRowsByColumn($table, $whereColumn, $whereValue, $fields ?: array_keys($expectedRow));
+        foreach ($rows as $row) {
+            $this->assertSame(
+                $expectedRow,
+                $row,
+                $message
+            );
+        }
+    }
+
+
+}
