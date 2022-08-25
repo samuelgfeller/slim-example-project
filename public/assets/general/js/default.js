f2eceb47a5083297bffe0317a238bc2f69972afc
Samuel Gfeller <contact@samuel-gfeller.ch>
2022-08-25 14:05:49 +0200

    Added auto resizable textarea to default JS [SLE-164]

--- public/assets/general/js/default.js
+++ public/assets/general/js/default.js
@@ -36,6 +36,14 @@ window.addEventListener("load", function (event) {
     }
 });
 
+// DOMContentLoaded faster than load as it doesn't wait on all resources
+window.addEventListener("DOMContentLoaded", function (event) {
+    /** Auto resize textareas */
+        // Target all textarea fields that have class name auto-resize-textarea
+    let textareasToResize = document.getElementsByClassName("auto-resize-textarea");
+    initAutoResizingTextareas(textareasToResize);
+});
+
 
 /**
  * Display flash messages to user
@@ -74,4 +82,50 @@ function slideFlashOut(flash) {
         flash.style.display = 'none';
         flash.remove();
     }, 800); // .slide-out animation is 0.9s
+}
+
+/**
+ * Auto resize all given textareas
+ * Source: https://stackoverflow.com/a/25621277/9013718
+ * Known issue: https://stackoverflow.com/q/73475416/9013718
+ *
+ * @param {object}textareas object array of textareas
+ */
+function initAutoResizingTextareas(textareas) {
+    // Init observer to call resizeTextarea when the dimensions of the textareas change
+    let observer = new ResizeObserver(entries => {
+        for (let entry of entries) {
+            // Call function to resize textarea with textarea element as "this" context
+            resizeTextarea.call(entry.target);
+        }
+    });
+    // Loop through textareas and add event listeners as well as other needed css attributes
+    for (const textarea of textareas) {
+        // Initially set height as otherwise the textarea is not high enough on load
+        textarea.style.height = textarea.scrollHeight.toString();
+        // Hide scrollbar
+        textarea.style.overflowY = 'hidden';
+        // Call resize function with "this" context once during initialisation as it's too high otherwise
+        resizeTextarea.call(textarea);
+        // Add event listener to resize textarea on input
+        textarea.addEventListener('input', resizeTextarea, false);
+        // Also resize textarea on window resize event binding textarea to be "this"
+        // window.addEventListener('resize', resizeTextarea.bind(textarea), false);
+        // Observe all text area resize change events (also works when scrollbar appears)
+        observer.observe(textarea);
+    }
+
+
+    function resizeTextarea() {
+        // Textareas have default 2px padding and if not set it returns 0px
+        let padding = window.getComputedStyle(this).getPropertyValue('padding-bottom');
+        // getPropertyValue('padding-bottom') returns "px" at the end it needs to be removed to be added to scrollHeight
+        padding = parseInt(padding.replace('px', ''));
+        // console.log('scrollHeight before height reset: '+this.scrollHeight);
+        // Reset textarea height to (supposedly) have correct scrollHeight afterwards
+        this.style.height = "auto";
+        // console.log('scrollHeight after height reset: ' + this.scrollHeight);
+        // Adapt height of textarea to new scrollHeight and padding
+        this.style.height = (this.scrollHeight + padding) + "px";
+    }
 }
