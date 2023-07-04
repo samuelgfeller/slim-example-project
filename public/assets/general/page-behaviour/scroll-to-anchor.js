636c5bdd2d70edfec579713dd97a165c590ee0a8
Samuel Gfeller <contact@samuel-gfeller.ch>
2023-07-04 13:54:33 +0200

    Fixed scroll to anchor for ressource loaded via xhr [SLE-164]

--- public/assets/general/page-behaviour/scroll-to-anchor.js
+++ public/assets/general/page-behaviour/scroll-to-anchor.js
@@ -3,14 +3,21 @@
  * if exists and remove anchor afterwards.
  * Source: https://stackoverflow.com/a/5298684/9013718
  */
-export function scrollToAnchor(){
+let anchor;
+
+export function scrollToAnchor() {
     // If anchor exists, scroll to the element
     let hash = window.location.hash.substring(1);
+    hash = hash !== '' ? hash : anchor;
     if (hash) {
         document.getElementById(hash)?.scrollIntoView({
-          behavior: 'smooth', // smooth scroll
-          block: 'start' // the upper border of the element will be aligned at the top of the visible part of the window of the scrollable area.
+            // smooth scroll
+            behavior: 'smooth',
+            // the upper border of the element will be aligned at the top of the visible part of the window of the scrollable area.
+            block: 'start'
         });
+        // Store hash in anchor var in case there are ressources that are loaded via xhr after initial page load (e.g. notes)
+        anchor = hash;
         // Remove hash sign but keep history entry (to replace history entry: replaceState)
         history.pushState("", document.title, window.location.pathname + window.location.search);
     }
