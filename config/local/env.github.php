972abed5583a23c0907da8d3547c0ced1d53d76a
Samuel Gfeller <contact@samuel-gfeller.ch>
2023-07-11 19:03:22 +0200

    Added mysql root password [SLE-184]

--- config/local/env.github.php
+++ config/local/env.github.php
@@ -10,4 +10,4 @@ require __DIR__ . '/env.test.php';
 $settings['db']['host'] = '127.0.0.1';
 $settings['db']['database'] = 'slim_example_project_test';
 $settings['db']['username'] = 'root';
-$settings['db']['password'] = '';
+$settings['db']['password'] = 'root';
