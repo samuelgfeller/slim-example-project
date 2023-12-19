dfdd7d61c07df5a93390286ae2410813768880f2
Samuel Gfeller <contact@samuel-gfeller.ch>
2023-12-19 09:50:33 +0100

    Test workflow APP_ENV [SLE-192]

--- config/env/env.github.php
+++ config/env/env.github.php
@@ -11,4 +11,4 @@ require __DIR__ . '/env.test.php';
 $settings['db']['host'] = '127.0.0.1';
 $settings['db']['database'] = 'slim_example_project_test';
 $settings['db']['username'] = 'root';
-$settings['db']['password'] = 'root2';
+$settings['db']['password'] = 'root';
