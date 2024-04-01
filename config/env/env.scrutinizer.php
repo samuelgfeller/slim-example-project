3e2ea263c6a448fd2367ec64b3a7e064d426f08c
samuelgfeller <contact@samuel-gfeller.ch>
2024-04-01 17:56:57 +0200

    Added Scrutinizer because screw Sonar [SLE-192]

---
+++ config/env/env.scrutinizer.php
@@ -0,0 +1,10 @@
+<?php
+
+// Must be "require" and not require_once
+require __DIR__ . '/env.test.php';
+
+// Database
+$settings['db']['host'] = '127.0.0.1';
+$settings['db']['database'] = 'slim_example_project_test';
+$settings['db']['username'] = 'root';
+$settings['db']['password'] = '';
